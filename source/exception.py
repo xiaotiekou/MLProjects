017772cb737927abb280f5739986e18ac7d21d07
Yefu Wang <yefuwang42@gmail.com>
2023-08-03 11:45:12 -0400

    EDA and Problem stating

--- source/exception.py
+++ source/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+from source.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()     #Gives you three important information
